b08559149dcee20c7bccc2393673f798bd4abef9
Dylan McLeod <mcleodd@berkeley.edu>
2022-04-22 13:26:34 -0700

    Fix training code (somewhat)

--- train_masked_autoencoder.py
+++ train_masked_autoencoder.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+import torch.utils.data as data
 
 seed = 42
 torch.manual_seed(seed)
@@ -18,58 +19,37 @@ learning_rate = 1e-3
 transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
 
 class AE(nn.Module):
-    def __init__(self, n_classes, hidden_dim, out_dim, layers, _lr, _momentum):
-        hidden_dim = 64
+    def __init__(self, in_dim, hidden_dim, out_dim, layers):
         self.model = nn.Sequential(
-            nn.BatchNorm2d(n_classes),
-            nn.Conv2D(in_channels=n_classes, out_channels=hidden_dim, kernel_size=1),
+            nn.BatchNorm2d(in_dim),
+            nn.Conv2D(in_channels=in_dim, out_channels=hidden_dim, kernel_size=1),
             nn.ReLu(),
             nn.BatchNorm2d(hidden_dim),
             *[nn.Sequential(nn.Conv2D(in_channels=hidden_dim,out_channels=hidden_dim,kernel_size=5,stride=2), nn.ReLu(), nn.BatchNorm2d(hidden_dim)) for _ in range(layers)],
-            nn.Conv2D(in_channels=hidden_dim,out_channels=out_dim,kernel_size=1)
-
-        )
-        self.mask_token = nn.Parameter(torch.randn(n_classes))
-        self.optim = torch.optim.SGD(self.model.parameters() + [mask_token], lr=_lr, momentum=_momentum)
+            nn.Conv2D(in_channels=hidden_dim,out_channels=out_dim,kernel_size=1))
 
     def forward(self, features):
         return model.forward(features)
         
-#TODO: LOAD FROM FILE
-test_dataset = torchvision.datasets.MNIST(
-    root="~/torch_datasets", train=False, transform=transform, download=True
-)
-
-test_loader = torch.utils.data.DataLoader(
-    test_dataset, batch_size=10, shuffle=False
-)
-
-train_dataset = torchvision.datasets.MNIST(
-    root="~/torch_datasets", train=True, transform=transform, download=True
-)
-
-train_loader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=batch_size, shuffle=True
-)
+train_data = torchvision.datasets.ImageFolder(root="Data/RogalandImages/elevation")
+train_loader = data.DataLoader(train_data, batch_size=40, shuffle=True,  num_workers=4)
+test_data = torchvision.datasets.ImageFolder(root="Data/RogalandImages/elevation")
+test_loader  = data.DataLoader(test_data, batch_size=40, shuffle=True, num_workers=4)
 
 #  use gpu if available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # create a model from `AE` autoencoder class
 # load it to the specified device, either gpu or cpu
-model = AE(1, 3, 1, 5, 1e-3, 0.9).to(device)
+model = AE(1, 64, 1, 5).to(device)
 
 
-# TODO: FIX TRAINING CODE
+optimizer = torch.optim.SGD(model.model.parameters(), lr=1e-3, momentum=0.9)
+criterion = nn.MSELoss()
 for epoch in range(epochs):
     loss = 0
     for batch_features, _ in train_loader:
-        # reshape mini-batch data to [N, 784] matrix
-        # load it to the active device
-        batch_features = batch_features.view(-1, 784).to(device)
-        
         # reset the gradients back to zero
-        # PyTorch accumulates gradients on subsequent backward passes
         optimizer.zero_grad()
         
         # compute reconstructions
@@ -94,31 +74,29 @@ for epoch in range(epochs):
     print("epoch : {}/{}, recon loss = {:.8f}".format(epoch + 1, epochs, loss))
     
 
-# TODO FIX/IMPLEENT PROPER TESTING CODE
 test_examples = None
 
 with torch.no_grad():
     for batch_features in test_loader:
         batch_features = batch_features[0]
-        test_examples = batch_features.view(-1, 784).to(device)
-        reconstruction = model(test_examples)
+        test_examples = batch_features.to(device)
+        reconstruction = model(test_examples)        
+        number = 10
+        plt.figure(figsize=(20, 4))
+        for index in range(number):
+            # display original
+            ax = plt.subplot(2, number, index + 1)
+            plt.imshow(test_examples[index].cpu().numpy().reshape(128, 128))
+            plt.gray()
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+
+            # display reconstruction
+            ax = plt.subplot(2, number, index + 1 + number)
+            plt.imshow(reconstruction[index].cpu().numpy().reshape(128, 128))
+            plt.gray()
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+        plt.show()
         break
         
-with torch.no_grad():
-    number = 10
-    plt.figure(figsize=(20, 4))
-    for index in range(number):
-        # display original
-        ax = plt.subplot(2, number, index + 1)
-        plt.imshow(test_examples[index].cpu().numpy().reshape(28, 28))
-        plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-        # display reconstruction
-        ax = plt.subplot(2, number, index + 1 + number)
-        plt.imshow(reconstruction[index].cpu().numpy().reshape(28, 28))
-        plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-    plt.show()
